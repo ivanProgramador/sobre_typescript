b68a6ae20eee152bdc8be0a039e754c734f22d4f
Ivan Rodrigues <ivan-guita@hotmail.com>
2024-12-16 18:49:33 -0300

    escolhendo tipos

---
+++ mostrando_valores.ts
@@ -0,0 +1,18 @@
+/*
+ Um recurso seria escolher oque sera recebido em uma função
+ usando como exemplo uma função de impressão que espera um string 
+ porém ela recebe um objeto.
+ 
+ se essa função não estiver pornta para tratar um objeto ela ate vai receber ele
+ sem problemas porém ela vai retonar algo inesperado na tela ou causa um erro dentro
+ do codigo da função.
+ 
+ No Typescript é possivel escolher oque a função vai receber atraves do tipo 
+ nesse exemplo eu digo que ela só recebe string
+
+ function imprimir(algo:string){
+    console.log(algo); 
+ }
+    
+*/
+
