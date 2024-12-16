c7012ea0b13114f7eb51491bfabc903776db56e3
Ivan Rodrigues <ivan-guita@hotmail.com>
2024-12-16 19:43:34 -0300

    usando o compilador ts de forma manual

---
+++ compilacao_manual.js
@@ -0,0 +1,2 @@
+var a = 'Teste ts';
+console.log(a);
---
+++ compilacao_manual.ts
@@ -0,0 +1,15 @@
+/*
+  Usando o comando 
+
+  tsc compilacao_manual.ts
+
+  O ts faz um scan no cadigo e gera um arquivo do tipo js  
+  como se fosse um tradução para javascrpt ou seja se eu tentar executar 
+  usando o node o ts vai retornar um erro porque ele só reconhece js por isso 
+  ele faz um codigo js simples 
+  
+*/
+
+
+const a:string = 'Teste ts'
+console.log(a);
