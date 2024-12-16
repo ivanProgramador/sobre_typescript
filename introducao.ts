31e9a9c4fa84e621d84d35537bf9ccdee1207484
Ivan Rodrigues <ivan-guita@hotmail.com>
2024-12-16 17:23:42 -0300

    introdução

---
+++ introducao.ts
@@ -0,0 +1,23 @@
+/*
+ Desenvolvida pela microsoft typescript é uma linguagem baseada em javascript
+ que acresenta mais bibiliotecas e funcionalidades ao javascript original como
+ se fosse um complemento.
+
+ O javascrpt lança versões mias atuais um exeplo seria a ES6 o porbelma e que 
+ nem todos os navegadores conseguem acompanhar essas atualizações então o TypeScript
+ consegue compilar para a versão que o navegador suporta,tornando possivel usar os 
+ recursos mais atuais quando necessário, assim o desenvolvedor não precisa esperar 
+ para que o navegador fique compativel com as novas features.
+
+ 
+
+
+
+ 
+ 
+
+
+
+
+
+*/
