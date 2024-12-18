f4a5defaed88e9593cb542241779fcce6ca70995
Ivan Rodrigues <ivan-guita@hotmail.com>
2024-12-18 13:57:58 -0300

    tipos array , enum

--- basico/tipos.ts
+++ basico/tipos.ts
@@ -12,4 +12,60 @@ let nome ='joão';
 
 nome=28
 
+atribuindo tipos de forma explicita isso pode feito dessa forma 
+
+let minhaIdade: number
+minhaIdade=27 
+
+dessa forma se eu quiser adicionar letras
+
+const minhaIdade:any 
+minhaIdade = "idade 27" 
 */
+
+/*
+Tipo array 
+*/
+/*
+Nesse caso a diretiva any significa que esse array pode  receber qualquer coisa 
+*/
+/*
+let hobbies:any[]= ['cozinhar','praticar esportes'];
+
+console.log(hobbies[0]); // aqui ele vai retornar o primeiro indice 
+
+console.log(typeof hobbies); //aqui eletenta identificar a que tipo pertence os hobies então ele vai dizer que é um objeto 
+*/
+
+/*
+ Tuplas ts 
+   A regra é 1 string e um numero por exemplo um endereço
+   como eu ja defini que quero um string e um numero
+   se outro tipo de dado entrar eu vou ter um problema
+      
+*/
+
+let endereco:[string,number,string] = ["Av principal",99,"bloco A"];
+
+console.log(endereco);
+
+/*
+ enums 
+   Trata-se de um tipo de objeto nativo que armazena valores fixos
+   se em uma aplicação tiver possibilidades fixas para um processo
+   por exemplo dias da semana , meses do ano eses dado podem ser definidos
+   dentro de um enum, depois e so chamar ele e apontar apar o valor fixo 
+   pre-definido   
+*/
+
+enum Cor{
+    Cinza,  // posição 1 
+    Verde = 100, // posição 2
+    Azul  //posição 3
+}
+
+let minhaCor = Cor.Verde;
+
+console.log(minhaCor);
+console.log(Cor.Azul);
+
